b5ef2f4a4d2829f75f9c3055b40fa6cb0a10ddc3
defalt1337 <brandonprog@windowslive.com>
2021-03-17 10:41:23 +0100

    replaced old js file

--- newwhat/js/index.js
+++ newwhat/js/index.js
@@ -1,4 +1,15 @@
-function onclick(){
+function message(){
     var get = document.getElementById("adat");
-    alert("Good morning" + get);
+    //alert("Good morning" + get);
+    var tomb = [];
+    for(var i = 0; i < get; i++){
+        var val = Math.floor(Math.random()*10)+1;
+        tomb[i] = val;
+    }
 }
+function init(){
+    //console.log("yo");
+    //document.getElementById("text").innerHTML += "nem tudom mi van";
+
+}
+window.addEventListener("load",init);
